2757c0e9e2a954ccd16b3eab63c5e27dea3fe6de
Farish Kashefinejad <Farish@Farishs-iMac.local>
2016-06-18 21:38:09 -0700

    add timer and countdown page

---
+++ app/components/Timer.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+
+
+var Timer = React.createClass({
+  render: function(){
+  return (
+    <div>
+      <p>Timer.jsx</p>
+    </div>
+  );
+}
+});
+
+
+module.exports = Timer;
